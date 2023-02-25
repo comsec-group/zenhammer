d00316f89c792dd04e77e295cb202d3631b2c36b
Patrick Jattke <pjattke@ethz.ch>
2023-02-25 17:45:55 +0100

    increases mappings-per-pattern to 3 and locations-pp. to 3

--- include/Blacksmith.hpp
+++ include/Blacksmith.hpp
@@ -9,8 +9,6 @@
 struct ProgramArguments {
   // the duration of the fuzzing run in second
   unsigned long runtime_limit = 120;
-  // the number of ranks of the DIMM to hammer
-  int num_ranks = 0;
   // no. of activations we can do within a refresh interval
   size_t acts_per_ref = 0;
   // path to JSON file to load
@@ -18,11 +16,9 @@ struct ProgramArguments {
   // the IDs of the patterns to be loaded from a given JSON file
   std::unordered_set<std::string> pattern_ids{};
   // total number of mappings (i.e., Aggressor ID -> DRAM rows mapping) to try for a pattern
-//  size_t num_address_mappings_per_pattern = 3;
-  size_t num_address_mappings_per_pattern = 1;
+  size_t num_address_mappings_per_pattern = 3;
   // number of DRAM locations we use to check a (pattern, address mapping)'s effectiveness
-//  size_t num_dram_locations_per_mapping = 3;
-  size_t num_dram_locations_per_mapping = 1;
+  size_t num_dram_locations_per_mapping = 3;
   // whether to sweep the 'best pattern' that was found during fuzzing afterward over a contiguous chunk of memory
   bool sweeping = false;
   // the ID of the DIMM that is currently inserted
